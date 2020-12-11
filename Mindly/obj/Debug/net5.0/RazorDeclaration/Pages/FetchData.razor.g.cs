68ac60500f5a107427612dd23a247073a19778ec
Rasmus Rimmi <rasmus136@gmail.com>
2020-12-11 13:31:13 +0200

    Signup/Login-Layout

--- Mindly/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ Mindly/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,83 +13,75 @@ namespace Mindly.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 1 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 2 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 3 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 4 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 5 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 6 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 7 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 8 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 9 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Mindly;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 10 "C:\Users\rasmu\Desktop\Mindly\Mindly-LoginLayout\Mindly\_Imports.razor"
 using Mindly.Shared;
 
 #line default
 #line hidden
 #nullable disable
-#nullable restore
-#line 3 "D:\git\Mindly\Mindly\Pages\FetchData.razor"
-using Mindly.Data;
-
-#line default
-#line hidden
-#nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/fetchdata")]
     public partial class FetchData : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -97,20 +89,6 @@ using Mindly.Data;
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 39 "D:\git\Mindly\Mindly\Pages\FetchData.razor"
-       
-    private WeatherForecast[] forecasts;
-
-    protected override async Task OnInitializedAsync()
-    {
-        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
-    }
-
-#line default
-#line hidden
-#nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private WeatherForecastService ForecastService { get; set; }
     }
 }
 #pragma warning restore 1591
