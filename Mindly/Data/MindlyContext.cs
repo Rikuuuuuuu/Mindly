4572d30119f80d85458c214d70c412d6053bcff9
Riku Ruotsalainen <gr189252@gradia.fi>
2021-01-26 10:52:40 +0200

    Update

---
+++ Mindly/Data/MindlyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mindly.Models;
+
+namespace Mindly.Data
+{
+    public class MindlyContext : DbContext
+    {
+        public MindlyContext (DbContextOptions<MindlyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mindly.Models.User> User { get; set; }
+    }
+}
---
+++ Mindly/Pages/Users/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mindly.Data;
+using Mindly.Models;
+
+namespace Mindly.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Mindly.Data.MindlyContext _context;
+
+        public DetailsModel(Mindly.Data.MindlyContext context)
+        {
+            _context = context;
+        }
+
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
