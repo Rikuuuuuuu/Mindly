0f5f2330b000bc46267c9ba7a42e27f18fa4f8d6
Rasmus Rimmi <rasmus136@gmail.com>
2021-02-08 10:55:38 +0200

    Authetication

--- Mindly/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ Mindly/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace Mindly.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 1 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 2 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 3 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 4 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 5 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 6 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 7 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 8 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 9 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Mindly;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\git\Mindly\Mindly\_Imports.razor"
+#line 10 "C:\Users\rasmu\source\repos\Mindly\Mindly\_Imports.razor"
 using Mindly.Shared;
 
 #line default
